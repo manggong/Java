9aa1ad9cc4bb8c125d97b742f93bcbec0ce4edf4
manggong <jihwank1995@gmail.com>
2021-12-23 21:53:36 +0900

    feat: Add selectAll

--- boost_course/daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
+++ boost_course/daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package kr.or.connect.daoexam.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+@ComponentScan(basePackages = { "kr.or.connect.daoexam.dao"})
 @Import({DBConfig.class})	 // 설정파일을 여러개로 나눔.
 public class ApplicationConfig {
 	
---
+++ boost_course/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
@@ -0,0 +1,5 @@
+package kr.or.connect.daoexam.dao;
+
+public class RoleDaoSqls {
+	public static final String SELECT_ALL = "select * from role";
+}
