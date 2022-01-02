f6bf3a9e819b07df287e5dd5721a106d9da846a3
manggong <jihwank1995@gmail.com>
2022-01-02 23:04:10 +0900

    feat: Add response

--- java_web/pro06/src/main/java/sec01/ex01/LoginServlet.java
+++ java_web/pro06/src/main/java/sec01/ex01/LoginServlet.java
@@ -1,6 +1,8 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,12 +28,23 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
 		
 		String user_id = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
 		
 		System.out.println("ID: " + user_id);
 		System.out.println("PW: " + user_pw);
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "ID" + user_id;
+		data += "PW" + user_pw;
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
 	}
 
 }
