39b9da5f996ef2cfca3725f30352c59d0b948d60
manggong <jihwank1995@gmail.com>
2022-01-02 22:48:09 +0900

    feat: Add servlet

---
+++ java_web/pro06/src/main/java/sec01/ex01/InputServlet.java
@@ -0,0 +1,62 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InputServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("ID: " + user_id);
+		System.out.println("PW: " + user_pw);
+		
+		String[] subject = request.getParameterValues("subject"); //하나의 이름으로 여러 값 전송하는 경우 
+		System.out.println("subject is " + Arrays.toString(subject));
+		for(String str: subject) {
+			System.out.println("checked subject is " + str);
+		}
+	}
+
+}
---
+++ java_web/pro06/src/main/java/sec01/ex01/LoginServlet.java
@@ -0,0 +1,37 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init!");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("ID: " + user_id);
+		System.out.println("PW: " + user_pw);
+	}
+
+}
