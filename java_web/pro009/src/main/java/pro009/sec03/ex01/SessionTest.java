faf45d1a74b0607eae8041b420d9bad5fdd8a8b7
manggong <jihwank1995@gmail.com>
2022-01-03 23:43:10 +0900

    feat: Add session

---
+++ java_web/pro009/src/main/java/pro009/sec03/ex01/SessionTest.java
@@ -0,0 +1,32 @@
+package pro009.sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/sess")
+public class SessionTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SessionTest() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		out.println("session id is " + session.getId() + "<br/>");
+		
+		if(session.isNew()) {
+			out.println("session is created");
+		}
+	}
+}
