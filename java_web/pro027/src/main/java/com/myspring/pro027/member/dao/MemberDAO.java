29169c158bb5f7c7fb1201ca02102c1a369ca3ca
manggong <jihwank1995@gmail.com>
2022-01-09 15:59:48 +0900

    feat: Add MemberDAO

---
+++ java_web/pro027/src/main/java/com/myspring/pro027/member/dao/MemberDAO.java
@@ -0,0 +1,13 @@
+package com.myspring.pro027.member.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.myspring.pro027.member.vo.MemberVO;
+
+public interface MemberDAO {
+	 public List selectAllMemberList() throws DataAccessException;
+	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
+	 public int deleteMember(String id) throws DataAccessException;
+}
