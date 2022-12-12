63d1b2e0ce218e772966199c3e962f5c8a191b53
manggong <jihwank1995@gmail.com>
2022-12-13 00:48:07 +0900

    feat: init project

---
+++ java_web/w1/src/main/java/org/zerock/w1/MyServelt.java
@@ -0,0 +1,27 @@
+package org.zerock.w1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "myServelt", value = "/my")
+public class MyServelt extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServelt</h1>");
+        out.println("</body></html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ java_web/w1/src/main/webapp/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kimjihwan
+  Date: 2022/12/13
+  Time: 12:34 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Test JSP PAGE</h1>
+</body>
+</html>
